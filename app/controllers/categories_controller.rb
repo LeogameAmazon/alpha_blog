523cc72288b167881b9cc14f09e04f326b14054c
LeogameAmazon <leogame@amazon.com>
2022-10-05 10:43:02 -0500

    We are testing the integration of categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,16 @@
 class CategoriesController < ApplicationController
     def new
-        
+        @category = Category.new
+    end
+
+    def create
+        @category = Category.new(category_params)
+        if @category.save
+            flash.keep[:notice] = "Category was successfully created"
+            redirect_to @category
+        else
+            render :new, status: :unprocessable_entity
+        end
     end
 
     def index
@@ -8,6 +18,25 @@ class CategoriesController < ApplicationController
     end
 
     def show
+        @category = Category.find(params[:id])
+    end
+
+    def edit
 
     end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+    private 
+
+    def category_params
+        params.require(:category).permit(:name)
+    end
+
 end
